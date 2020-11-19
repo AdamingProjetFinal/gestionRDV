c4f62943945ce3537bb09d95a6ad2bd8b8fe52b2
damscheb@gmail.com <damscheb@gmail.com>
2020-11-19 16:11:23 +0100

    [correction] for microservice

--- src/main/java/com/GestionRdvApplication.java
+++ src/main/java/com/GestionRdvApplication.java
@@ -1,8 +1,17 @@
 package com;
 
+import java.util.stream.Stream;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
+import com.entities.Medecin;
+import com.service.MedecinService;
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GestionRdvApplication {
 
