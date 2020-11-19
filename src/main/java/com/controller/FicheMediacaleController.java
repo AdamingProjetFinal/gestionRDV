09f407dcfa18087fea16c587a14ee7985d2d689d
atomystic <gnavo.tony@hotmail.fr>
2020-11-19 10:56:16 +0100

    [add] FicheMedicale

---
+++ src/main/java/com/controller/FicheMediacaleController.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Tony
+ *
+ */
+@RequestMapping("/ficheMedicale")
+@CrossOrigin("*")
+public class FicheMediacaleController {
+
+}
---
+++ src/main/java/com/repository/FicheMedicaleRepository.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.repository;
+
+import org.springframework.stereotype.Repository;
+
+import com.entities.FicheMedicale;
+import com.service.DaoService;
+
+/**
+ * @author Tony
+ *
+ */
+@Repository
+public interface FicheMedicaleRepository extends DaoService<FicheMedicale> {
+
+}
