d48427bc04e0a8f2f60abb1c5f9282dd4d81d9af
damscheb@gmail.com <damscheb@gmail.com>
2020-11-20 10:31:20 +0100

    [add] swagger2

--- src/main/java/com/controller/PatientController.java
+++ src/main/java/com/controller/PatientController.java
@@ -3,7 +3,9 @@
  */
 package com.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.entities.Patient;
 
@@ -11,8 +13,9 @@ import com.entities.Patient;
  * @author Tony
  *
  */
+@RestController
 @RequestMapping(value ="/patient")
-
+@CrossOrigin(origins = "*")
 public class PatientController extends ControllerImpl<Patient> {
 
 }
